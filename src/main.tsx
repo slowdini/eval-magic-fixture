ea4e7ab3a80e37e6c8b17cfe1b93303113dfb0c3
samiamorwas <samiamorwas@gmail.com>
2026-08-25 00:55:39 -0400

    refactor: separate responsive styles

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom'
 
 import { App } from './app/App'
 import './styles.css'
+import './responsive.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
